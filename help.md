cd3a31858849dda6016a0939b3d60e8df875fe80
TylerBeach <TylerAllenBeach@gmail.com>
2025-01-03 22:19:56 -0700

    Remove Oil + fix lsp issues

---
+++ help.md
@@ -0,0 +1,11 @@
+# Commands for NVIM Config 
+
+## Open Treesitter 
+ctrl + n 
+
+## File search 
+ctrl + p 
+space + space ( leader + leader ) 
+
+## Open Files
+space(leader) + b + f 
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,33 +10,46 @@ return {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
     opts = {
-      auto_install = true,
+      auto_install = true, -- Automatically install configured language servers
     },
   },
   {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      -- Helper function for on_attach
+      local function on_attach(_, bufnr)
+        local opts = { buffer = bufnr }
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+      end
+
+      -- Configure language servers
+      local servers = {
+        tsserver = {},
+        solargraph = {},
+        html = {},
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = { globals = { "vim" } }, -- Avoid 'undefined global' warnings for vim workspace = { checkThirdParty = false },
+            },
+          },
+        },
+      }
+
+      for server, config in pairs(servers) do
+        lspconfig[server].setup(vim.tbl_extend("force", {
+          capabilities = capabilities,
+          on_attach = on_attach,
+        }, config))
+      end
     end,
   },
 }
+
